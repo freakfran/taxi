ae503e04704168591def536711abd73c39a4a546
fran <2723579248@qq.com>
2023-04-14 17:57:15 +0800

    分布式并发，redis锁

--- service-order-8989/src/main/java/com/fran/config/RedisConfig.java
+++ service-order-8989/src/main/java/com/fran/config/RedisConfig.java
@@ -3,15 +3,24 @@ package com.fran.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+    private String potocol = "redis://";
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+
     @Bean
     public RedissonClient redisson(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        config.useSingleServer().setAddress(potocol + redisHost + ":" + redisPort).setDatabase(0);
         return Redisson.create(config);
     }
 }
