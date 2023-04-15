e2f9b085f951e244e4dbdd8a4e0ca925ca69add1
fran <2723579248@qq.com>
2023-04-15 11:41:55 +0800

    客户端连接建立，消息推送，连接关闭

---
+++ sse-driver-client-web/src/main/java/com/fran/SseDriverMain9000.java
@@ -0,0 +1,11 @@
+package com.fran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseDriverMain9000 {
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverMain9000.class);
+    }
+}
